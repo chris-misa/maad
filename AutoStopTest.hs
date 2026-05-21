a5137c5601a7a5cbd337a96c65096f4e432ec5b0
Chris Misa <cmisa@cs.uoregon.edu>
2026-05-21 11:31:36 -0700

    comments

--- AutoStopTest.hs
+++ AutoStopTest.hs
@@ -55,24 +55,16 @@ run inputfile = do
       extractSingleAddr (_:addr:_) = addr
       extractSingleAddr [] = error "Expected at least one column in each input row"
 
-  -- The problem is that this runs increasingly slower as more and more addresses are added.
-  -- There's probably a way to combine PM.insert with PM.firstSpilloverLength...
-  -- but it requires a bit of accounting on the prefix tree? or a min operation? or something?
-
-  -- Core intuition: when things start spilling over, enough of the structure is already present.
-  -- trickiness about order: if something spills over in one part of the space, how do you know the rest of the space is sufficiently present?
+  -- TODO: modify to also report the local spill-over prefix length?
   
-
-  -- Did confirm that the idea seems to hold: as more prefixes are added it gradually goes to shorter prefix lengths...
-
-  -- Could also use some kind of search thing...
-  -- Or just batches?
   let processOne :: Int -> PrefixMap Double -> [[B.ByteString]] -> IO ()
       processOne idx pfxs (row : theRest) =
         let addr = extractSingleAddr row in
         case PM.lookup (PM.addressToPrefix (string_to_ipv4 addr)) pfxs of
 	  Nothing -> do
             let (len, pfxs') = PM.insertNoDupLen pfxs (string_to_ipv4 addr, 1.0)
+            -- look up parent at len
+            -- if it's more than threshold full, update spill-over length (min)
             putStrLn $ (show idx) ++ "," ++ (show len)
             processOne (idx + 1) pfxs' theRest
 	  Just _ -> processOne idx pfxs theRest
