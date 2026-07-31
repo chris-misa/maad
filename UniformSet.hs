6cecaced2cd2d3782e7e23e6f64f2e91c8a6e942
Chris Misa <cmisa@uoregon.edu>
2026-07-31 19:40:16 +0800

    added uniform generation utility

---
+++ UniformSet.hs
@@ -0,0 +1,41 @@
+{-
+ - Copyright: 2026 Chris Misa
+ - License: (See ./LICENSE)
+ -
+ - Generate a set of synthetic IPv4 addresses drawn from a uniform distribution over [0,2^32 - 1]
+ -}
+
+module UniformSet where
+
+import System.Environment
+
+import Data.Word
+import qualified Data.ByteString.Char8 as B
+import Data.Function ((&))
+import Data.List (unfoldr)
+
+import System.Random (mkStdGen, genWord32)
+
+import Common
+
+usage :: String
+usage = "UniformSet <random seed> <number of addresses>"
+
+writeSet :: Int -> Int -> IO ()
+writeSet seed n = do
+  unfoldr (Just . genWord32) (mkStdGen seed)
+    & take n
+    & mapM_ (\ip -> do
+                B.putStrLn (ipv4_to_string ip)
+            )
+  return ()
+  
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [seed, n] -> do
+      writeSet (read seed) (read n)
+    _ -> putStrLn usage
+        
