e518cb2b99d78c8d28eba8d33e0e223dbc105d55
Chris Misa <cmisa@uoregon.edu>
2026-09-25 02:52:51 +0800

    move PrefixCounts.hs to be baked-in csv format, better handling of v6 prefix trees, cleaned up compile.sh

--- PrefixMap.hs
+++ PrefixMap.hs
@@ -378,11 +378,14 @@ shouldStop n pfxs@(Node (Prefix addr _) _ _ _ _) target_width = do
         Addr4 _ -> 30
         Addr6 _ -> 126
 
-      -- TODO: for v6 we have to start at /12 or something because they all have the same first bits!
+      minLen = case addr of
+        Addr4 _ -> 1
+        Addr6 _ -> 23
+
       prefixCounts :: [(Int, Int)]
       prefixCounts = [
         (pl, length $ leaves $ sliceAtLength pl pfxs)
-        | pl <- [1..maxLen]
+        | pl <- [minLen..maxLen]
         ]
 
       subCount :: (Int, Int) -> (Int, Int) -> (Int, Int)
